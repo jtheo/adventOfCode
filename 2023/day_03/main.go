f7430c91b37b3b1926c6be7e9568cfaeef3e811a
Matteo Marchelli <matteo.marchelli@gmail.com>
2023-12-04 22:25:45 +0000

    day 3, part 1 fixed

--- 2023/day_03/main.go
+++ 2023/day_03/main.go
@@ -17,16 +17,9 @@ func main() {
 func part1(input [][]string) int {
 	res := 0
 	for i, l := range input {
-		// fmt.Println(i, l, len(l))
 		s, e := 0, 0
 		isNum := false
 		rightStop := len(l) - 1
-		fmt.Print("   ")
-		for i := range l {
-			fmt.Printf("%d ", i)
-		}
-		fmt.Println()
-		fmt.Println(i, l)
 
 		for x, c := range l {
 			if c >= "0" && c <= "9" {
@@ -49,7 +42,6 @@ func part1(input [][]string) int {
 					if err != nil {
 						log.Fatalf("converting %q to int, got %v\ns: %d, e: %d, l: %d, rightStop: %d\n", valid, err, s, e, i, rightStop)
 					}
-					fmt.Println("found", n)
 					res += n
 				}
 			}
@@ -61,7 +53,7 @@ func part1(input [][]string) int {
 func lookAroundForSymbols(i, s, e int, matrix [][]string) bool {
 	hLimit, vLimit := len(matrix[0])-1, len(matrix)-1
 	up, down := i-1, i+1
-	left, right := s-1, e+1
+	left, right := s-1, e
 	if up < 0 {
 		up = 0
 	}
