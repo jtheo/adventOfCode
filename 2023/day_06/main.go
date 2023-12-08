ce658f574c7a69f8f0f1305910ec4d83a73decf6
Matteo Marchelli <matteo.marchelli@gmail.com>
2023-12-08 12:21:57 +0000

    day 6, part 1 and 2 working

--- 2023/day_06/main.go
+++ 2023/day_06/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -9,9 +10,41 @@ func main() {
 
 	res := part1(r)
 	fmt.Printf("Day 6, part 1. Result: %d\n\n", res)
+
+	res = part2(r)
+	fmt.Printf("Day 6, part 2. Result: %d\n\n", res)
 }
 
 func part1(r races) int {
-	min := 0
-	return min
+	solutions := make([]int, len(r))
+	for c, x := range r {
+		for i := 1; i < x.time; i++ {
+			timeToRun := x.time - i
+			distance := timeToRun * i
+			if distance > x.distance {
+				solutions[c]++
+			}
+		}
+	}
+	res := 1
+	for _, s := range solutions {
+		res *= s
+	}
+	return res
+}
+
+func part2(r races) int {
+	tmpTime := ""
+	tmpDist := ""
+	for _, x := range r {
+		tmpTime += fmt.Sprintf("%d", x.time)
+		tmpDist += fmt.Sprintf("%d", x.distance)
+	}
+	time, _ := strconv.Atoi(tmpTime)
+	dist, _ := strconv.Atoi(tmpDist)
+	newRace := races{
+		{time: time, distance: dist},
+	}
+	res := part1(newRace)
+	return res
 }
